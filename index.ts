aacb00be49ee8fa5ce71cd047966850a3ec27061
Yaroslav <q.link0.p@gmail.com>
2021-12-03 14:57:38 +0200

    added relative leaderboard position change to message

--- index.ts
+++ index.ts
@@ -17,7 +17,80 @@ client.once("ready", () => {
     console.log("Application is ready and goin'")
 })
 
-interface Member {
+interface MemberCore {
+    id: string
+    name?: string
+    localScore: number
+    globalScore: number
+    stars: string
+    lastStarTs?: number
+}
+
+interface Member extends MemberCore {
+    completionDayLevel: Record<
+        number,
+        {
+            1: { get_star_ts: number }
+            2?: { get_star_ts: number }
+        }
+    >
+}
+
+interface MemberDay extends MemberCore {
+    day: number
+    firstStar?: number
+    secondStar?: number
+}
+
+interface MemberFirstStar extends MemberDay {
+    firstStar: number
+}
+
+interface MemberSecondStar extends MemberFirstStar {
+    secondStar: number
+}
+
+const solvedFirst = (member: MemberDay): member is MemberFirstStar =>
+    member.firstStar !== undefined
+const solvedSecond = (member: MemberDay): member is MemberSecondStar =>
+    member.secondStar !== undefined
+const memberForDay =
+    (day: number) =>
+    ({ completionDayLevel, ...core }: Member): MemberDay => ({
+        ...core,
+        day,
+        firstStar: completionDayLevel[day]?.[1].get_star_ts,
+        secondStar: completionDayLevel[day]?.[2]?.get_star_ts,
+    })
+const leaderboardForDay = (
+    { members, ...rest }: Leaderboard,
+    day: number
+): LeaderboardDay => ({
+    ...rest,
+    day,
+    members: members.map(memberForDay(day)),
+})
+
+interface Leaderboard {
+    ownerId: string
+    event: string
+    members: Member[]
+}
+
+interface LeaderboardDay {
+    day: number
+    ownerId: string
+    event: string
+    members: MemberDay[]
+}
+
+class ResponseError extends Error {
+    constructor(public status: number, public response: string) {
+        super(`Error response from advent of code: ${status} - ${response}`)
+    }
+}
+
+interface APIMember {
     id: string
     name: string | null
     local_score: number
@@ -33,17 +106,27 @@ interface Member {
     >
 }
 
-interface Leaderboard {
+interface APILeaderboard {
     owner_id: string
     event: string
-    members: Record<string, Member>
+    members: Record<string, APIMember>
 }
 
-class ResponseError extends Error {
-    constructor(public status: number, public response: string) {
-        super(`Error response from advent of code: ${status} - ${response}`)
-    }
-}
+const toDomainMember = (member: APIMember): Member => ({
+    id: member.id,
+    globalScore: member.global_score,
+    localScore: member.local_score,
+    lastStarTs: member.last_star_ts !== "0" ? member.last_star_ts : undefined,
+    name: member.name || undefined,
+    stars: member.stars,
+    completionDayLevel: member.completion_day_level,
+})
+
+const toDomainLeaderboard = (lb: APILeaderboard): Leaderboard => ({
+    ownerId: lb.owner_id,
+    event: lb.event,
+    members: Object.values(lb.members).map(toDomainMember),
+})
 
 async function fetchLeaderboard(
     leaderboardId: string,
@@ -62,62 +145,146 @@ async function fetchLeaderboard(
     if (!res.ok) {
         throw new ResponseError(res.status, await res.text())
     }
-    return res.json() as Promise<Leaderboard>
+    const leaderboard = (await res.json()) as APILeaderboard
+    return toDomainLeaderboard(leaderboard)
 }
 
 const formatTime = (timestamp: number) =>
     dateformat(new Date(timestamp * 1000), "H:MM")
 
-const cmpMembersByDay = (day: number) => (a: Member, b: Member) => {
-    const aSndStar = a.completion_day_level[day][2]?.get_star_ts
-    const bSndStar = b.completion_day_level[day][2]?.get_star_ts
-    if (aSndStar && bSndStar) {
-        return aSndStar - bSndStar
+const cmpMembers = (a: MemberFirstStar, b: MemberFirstStar) => {
+    if (a.secondStar && b.secondStar) {
+        return a.secondStar - b.secondStar
     }
-    if (aSndStar) {
+    if (a.secondStar) {
         return -1
     }
-    if (bSndStar) {
+    if (b.secondStar) {
         return 1
     }
-    const aFstStar = a.completion_day_level[day][1].get_star_ts
-    const bFstStar = a.completion_day_level[day][1].get_star_ts
-    return aFstStar - bFstStar
+    return a.firstStar - b.firstStar
 }
 
-function formatLeaderboard(leaderboard: Leaderboard, day: number) {
-    const activeMembers = Object.values(leaderboard.members).filter(
-        member => member.completion_day_level[day]
+const cmpFirstStar = (a: MemberFirstStar, b: MemberFirstStar) =>
+    a.firstStar - b.firstStar
+
+const cmpSecondStar = (a: MemberSecondStar, b: MemberSecondStar) =>
+    a.secondStar - b.secondStar
+
+const cmpLocalScore = (a: MemberCore, b: MemberCore) =>
+    a.localScore - b.localScore
+
+interface EntryFormatOptions {
+    position: number
+    totalEntries: number
+    largestName?: number
+    leaderboardPosition: number
+    previousLeaderboardPosition?: number
+}
+
+function formatPositionChange(
+    leaderboardPosition: number,
+    previousLeaderboardPosition: number,
+    totalEntries: number,
+) {
+    const change = previousLeaderboardPosition - leaderboardPosition
+
+    const positionSpacesRequired =
+        Math.log10(totalEntries) - Math.floor(Math.log10(leaderboardPosition)) + 1
+    const positionSpacing = "".padEnd(positionSpacesRequired)
+
+    if (change === 0) {
+        return `${leaderboardPosition}`
+    }
+    if (change > 0) {
+        return `${leaderboardPosition}${positionSpacing}(🔺 +${change})`
+    }
+    return `${leaderboardPosition}${positionSpacing}(🔻 ${change})`
+}
+
+function formatEntry(
+    member: MemberFirstStar,
+    {
+        position,
+        totalEntries,
+        largestName = 0,
+        leaderboardPosition,
+        previousLeaderboardPosition = 0,
+    }: EntryFormatOptions
+) {
+    const name = (member.name || `<anonymous ${member.id}>`).padEnd(largestName)
+    const firstStarTime = formatTime(member.firstStar).padEnd(5)
+    const secondStarText = member.secondStar
+        ? `    🌟: ${formatTime(member.secondStar).padEnd(5)}`
+        : "             "
+    const positionSpacesRequired =
+        Math.log10(totalEntries) - Math.floor(Math.log10(position)) + 1
+    const positionSpacing = "".padEnd(positionSpacesRequired)
+    const positionChange = formatPositionChange(
+        leaderboardPosition,
+        previousLeaderboardPosition,
+        totalEntries
     )
+
+    return `${position}.${positionSpacing}${name} ⭐️: ${firstStarTime}${secondStarText}    ${positionChange}`
+}
+
+function formatLeaderboard({ members, day }: LeaderboardDay) {
+    const activeMembers = Object.values(members).filter(solvedFirst)
     const largestName = Math.max(
         ...activeMembers.map(member => {
             const name = member.name || `<anonymous ${member.id}>`
             return name.length
         })
     )
+    const scoresForToday = computeScores(members)
+    const positionForToday = new Map(
+        members
+            .sort(cmpLocalScore)
+            .map((member, idx) => [member.id, members.length - idx] as const)
+    )
+    const positionsForYesterday = new Map(
+        members
+            .map(member => ({
+                ...member,
+                localScore: member.localScore - scoresForToday[member.id],
+            }))
+            .sort(cmpLocalScore)
+            .map((member, idx) => [member.id, members.length - idx] as const)
+    )
 
-    const formattedLeaderboard = Object.values(leaderboard.members)
-        .filter(member => member.completion_day_level[day])
-        .sort(cmpMembersByDay(day))
-        .map((member, idx) => {
-            const name = member.name || `<anonymous ${member.id}>`
-            const firstStarTime = formatTime(
-                member.completion_day_level[day][1].get_star_ts
-            ).padEnd(5, " ")
-            const nameSpaces = Array(largestName - name.length)
-                .fill(" ")
-                .join("")
-            const secondTimestamp =
-                member.completion_day_level[day][2]?.get_star_ts
-            const secondStarText = secondTimestamp
-                ? `    🌟: ${formatTime(secondTimestamp)}`
-                : ""
-            const pos = idx + 1
-            return `${pos}. ${name}${nameSpaces} ⭐️: ${firstStarTime}${secondStarText}`
-        })
+    const formattedLeaderboard = activeMembers
+        .sort(cmpMembers)
+        .map((member, idx) =>
+            formatEntry(member, {
+                position: idx + 1,
+                totalEntries: activeMembers.length,
+                largestName,
+                leaderboardPosition: positionForToday.get(member.id)!,
+                previousLeaderboardPosition: positionsForYesterday.get(
+                    member.id
+                ),
+            })
+        )
         .join("\n")
 
     return `Todays ranking (day ${day}):\`\`\`\n${formattedLeaderboard}\`\`\``
+}
+
+function computeScores(members: MemberDay[]) {
+    const whoSolvedFirst = members.filter(solvedFirst)
+    const firstStarScores = whoSolvedFirst
+        .sort(cmpFirstStar)
+        .map((member, idx) => [member, members.length - idx] as const)
+    const secondStarScores = whoSolvedFirst
+        .filter(solvedSecond)
+        .sort(cmpSecondStar)
+        .map((member, idx) => [member, members.length - idx] as const)
+    const scores = Object.fromEntries(members.map(_ => [_.id, 0]))
+    for (const [{ id }, score] of [...firstStarScores, ...secondStarScores]) {
+        scores[id] += score
+    }
+    return scores
 }
 
 const competitionStart = new Date(2021, 11, 1, 5, 0)
@@ -134,9 +301,11 @@ client.on("interactionCreate", async interaction => {
     switch (interaction.commandName) {
         case "today":
             const leaderboard = await fetchLeaderboard(leaderboardId, 2021)
-            interaction.reply(
-                formatLeaderboard(leaderboard, currentCompetitionDay())
+            const forToday = leaderboardForDay(
+                leaderboard,
+                currentCompetitionDay()
             )
+            interaction.reply(formatLeaderboard(forToday))
             break
         case "default":
             console.warn(
